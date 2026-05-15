5472ac782e0ab90a4e3773e309cafaba35cbfd1f
Jakob <202007550@post.au.dk>
2026-05-15 22:29:29 +0200

    clean capture image

--- Final Project/pipeline/captureImage.m
+++ Final Project/pipeline/captureImage.m
@@ -1,17 +1,7 @@
-function captured = captureImage()
-% Save the latest camera frame, reusing detectCircle's subscriber (global img).
-    global img new_image_available %#ok<GVMIS>
-
-    wait_timer = tic;
-    while (isempty(img) || ~new_image_available) && toc(wait_timer) < 5
-        pause(0.05);
-    end
-    if isempty(img)
-        error('no camera frame received');
-    end
-
-    captured = rot90(img, 2);
+function captureImage()
+    global img
+    frame = rot90(img, 2);
     filename = ['picture_', char(datetime('now', 'Format', 'yyyyMMdd_HHmmss')), '.png'];
-    imwrite(captured, filename);
+    imwrite(frame, filename);
     fprintf('Picture saved: %s\n', filename);
 end
